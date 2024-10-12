d1e66256ef38f1849eddaae0116f62257a9c71c8
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-10-12 20:50:17 +0900

    react 자동 import 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       env: {
         node: true,
-        browser : true,
+        browser: true,
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
